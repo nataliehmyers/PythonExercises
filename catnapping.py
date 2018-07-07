03375a489c15278bad6435eb6a7e05e89cf3de40
Natalie Myers <nataliehmyers@gmail.com>
2018-07-07 16:36:44 -0700

    Using a multiline string with triple quotes to print a letter

---
+++ catnapping.py
@@ -0,0 +1,11 @@
+print('''Dear Alice,
+
+Eve's cat has been arrested for catnapping, cat burglary, and extortion.
+
+Sincerely,
+Bob''')
+
+
+#  this prints identical text but does not use a multiline string
+
+#  print('Dear Alice,\n\nEve\'s cat has been arrested for catnapping, cat burglary, and extortion.\n\nSincerely,\nBob')
