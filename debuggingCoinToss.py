d22448ca5d8fa8e64b7fd4e35b294887955ef49a
Natalie Myers <nataliehmyers@gmail.com>
2018-07-18 01:09:08 -0700

    A simple coin toss guessing game that has several bugs fixed

---
+++ debuggingCoinToss.py
@@ -0,0 +1,21 @@
+#! /usr/bin/env python3
+
+import random
+guess = ''
+while guess not in ('heads', 'tails'):
+    print('Guess the coin toss! Enter heads or tails:')
+    guess = input()
+toss = random.randint(0, 1) # 0 is tails, 1 is heads
+if toss == 0 and guess == 'tails':
+    print('You got it!')
+elif toss == 1 and guess == 'heads':
+    print('You got it!')
+else:
+    print('Nope! Guess again!')
+    guess = input()
+    if toss == 0 and guess == 'tails':
+        print('You got it!')
+    elif toss == 1 and guess == 'heads':
+        print('You got it!')
+    else:
+        print('Nope. You are really bad a this game.')
