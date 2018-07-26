9d13ae8a7039abb8b40224661a05b2d4ad50852d
Natalie Myers <nataliehmyers@gmail.com>
2018-07-26 12:02:19 -0700

    Opens a Word document, loops over all the Paragraph objects in the paragraphs list, and then appends their text to the list in fullText

---
+++ readDocx.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python3
+
+import docx
+
+def getText(filename):
+    doc = docx.Document(filename)
+    fullText = []
+    for para in doc.paragraphs:
+        fullText.append(' ' + para.text)
+    return '\n\n'.join(fullText)
