490b7e7fdecb983b99a7caa1a329b0f89b47ef0c
Natalie Myers <nataliehmyers@gmail.com>
2018-07-31 14:21:52 -0700

    An example of a dual-thread program

---
+++ threadDemo.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python3
+
+import threading, time
+print('Start of program.')
+
+def takeANap():
+    time.sleep(5)
+    print('Wake up!')
+
+threadObj = threading.Thread(target=takeANap)
+threadObj.start()
+
+print('End of program.')
